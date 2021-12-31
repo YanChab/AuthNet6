35264d8a8f695aa243109801130e23429aa2dc32
YanChab <ychabr@gmail.com>
2021-12-31 15:06:11 +0100

    sauve

---
+++ Shared/JobModel.cs
@@ -0,0 +1,18 @@
+namespace AuthNet6.Shared
+{
+    public class JobModel
+    {
+        public int Id { get; set; }
+        public JobStatuses Status { get; set; }
+        public string Description { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+
+    public enum JobStatuses
+    {
+        Todo,
+        Started,
+        Completed,
+        Canceled
+    }
+}
